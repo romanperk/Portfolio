62c533eeaae912dfc2f947b09afad4d38a51d4d3
Roman Perk PC <romanperk@seznam.cz>
2025-04-01 20:07:44 +0200

    changed order of experience

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -5,21 +5,21 @@ import { PageHeader } from "../components/PageHeader";
 export const Experience = () => {
   const timeLineData = {
     dates: [
-      "October, 2021 - June, 2022",
-      "September, 2022 - expected June, 2025",
       "July, 2024 - Present",
+      "September, 2022 - expected June, 2025",
+      "October, 2021 - June, 2022",
     ],
     headers: [
-      "Promoter for Acer and Microsoft at Alza.cz",
-      "SW Development student at Unicorn University",
       "Part-time Junior React Developer at 2N Telekomunikace a.s.",
+      "SW Development student at Unicorn University",
+      "Promoter for Acer and Microsoft at Alza.cz",
     ],
     texts: [
-      "Work included training on new products regarding the brand Acer, Microsoft and, for example, Intel.",
-      "I am a 3rd year student at Unicorn University, majoring in Software Development." +
-        " I am gaining knowledge in fields such as React.js, JavaScript, Java, AI, Project Management, Mathematics etc. I have worked with my colleagues on a few group projects (fe. video library and temperature tracking using IoT device). Currently doing a bachelor's work on Comparison of component libraries for development in React.",
       "The role involves contributing to Frontend codebase of the My2N website, cooperating with the team (Backend, QA, Scrum master, PO)" +
         " using Jira and Agile form of development (Stand-ups, Refinements, Estimations, User Story mappings etc.). The stack includes React in JS (new code in TS) with MUI, React Hook Form, Redux, Redux Toolkit, Jest testing library and GitLab.",
+      "I am a 3rd year student at Unicorn University, majoring in Software Development." +
+        " I am gaining knowledge in fields such as React.js, JavaScript, Java, AI, Project Management, Mathematics etc. I have worked with my colleagues on a few group projects (fe. video library and temperature tracking using IoT device). Currently doing a bachelor's work on Comparison of component libraries for development in React.",
+      "Work included training on new products regarding the brand Acer, Microsoft and, for example, Intel.",
     ],
   };
 
